deb11c828006c1446f7ef92bcb298f2708f550a3
zhaohaoyang <652246564@qq.com>
2019-10-14 16:17:45 +0800

    添加ProfilingNGUI

---
+++ Assets/Scripts/TestIsPlaying.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestIsPlaying : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.isPlaying)
+        {
+            int val = 1;
+        }
+        else
+        {
+            int val = 1;
+        }
+
+        if (Application.isEditor)
+        {
+            int val = 1;
+        }
+        else
+        {
+            int val = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int val = 1;
+    }
+}
